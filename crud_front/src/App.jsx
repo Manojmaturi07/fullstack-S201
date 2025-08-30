e0e55b722c34af964596ee1fc32e8e3416a27c54
Manojmaturi07 <162708025+Manojmaturi07@users.noreply.github.com>
2025-08-30 08:26:17 +0530

    Update App.jsx

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://localhost:8080/springapp1';
+const BASE_URL = 'http://3.89.174.92:8080/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
